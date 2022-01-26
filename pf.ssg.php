f7cf95f17b7af513b03d1231002ef4cc6c1b96fb
seoyoenkk68 <seoyeonkk68@gmail.com>
2022-01-26 15:23:05 +0900

    포폴추가

--- pf.ssg.php
+++ pf.ssg.php
@@ -12,8 +12,9 @@ $pageCode="pf";
         <div class="txt-title">Portfolio</div>
         <div class="flex-grow"></div>
         <ul class="flex">
-          <li data-tb="tab-box-1__1__head" data-tb-clicked><a href="#" class="flex p-5 font-bold">Web/Mobile</a></li>
-          <li data-tb="tab-box-1__2__head"><a href="#" class="flex p-5 font-bold">Design</a></li>
+          <li data-tb="tab-box-1__1__head" data-tb-clicked><a href="#" class="flex p-5 font-bold">All</a></li>
+          <li data-tb="tab-box-1__2__head"><a href="#" class="flex p-5 font-bold">Web/Mobile</a></li>
+          <li data-tb="tab-box-1__3__head"><a href="#" class="flex p-5 font-bold">Design</a></li>
           <!--           <li data-tb="tab-box-1__3__head"><a href="#" class="flex p-5 font-bold">artwork</a></li> -->
         </ul>
       </div>
@@ -133,9 +134,167 @@ $pageCode="pf";
                 </div>
                 <div class="img img-8"></div>
               </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull1.png"
+                  data-lightbox="image-1" data-title="일러스트레이터 실습 - 영화 인셉션 아트 포스터">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork1.png"
+                    alt="">
+                </a>
+              </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull2.png"
+                  data-lightbox="image-1" data-title="포토샵 실습 - 사진전 포스터 - 1">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork2.png"
+                    alt="">
+                </a>
+              </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull3.png"
+                  data-lightbox="image-1" data-title="포토샵 실습 - 사진전 포스터 - 2">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork3.png"
+                    alt="">
+                </a>
+              </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull4.png"
+                  data-lightbox="image-1" data-title="overcomplex 로고 시안 1">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork4.png"
+                    alt="">
+                </a>
+              </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull5.png"
+                  data-lightbox="image-1" data-title="overcomplex 로고 시안 2">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork5.png"
+                    alt="">
+                </a>
+              </li>
+              <li>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull6.png"
+                  data-lightbox="image-1" data-title="박찬미 찬양콘서트 포스터">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork6.png"
+                    alt="">
+                </a>
+              </li>
             </ul>
           </li>
           <li data-tb="tab-box-1__2__body">
+            <ul class="list flex">
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">한글과 컴퓨터 말랑말랑 웹페이지 모작</span>
+                    <span class="content">작업기간 2021.05.29~6.13</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://codepen.io/sbyeony/full/ExWOamb" target="_blank" class="btn btn-1">codepen
+                      open</a>
+                    <!-- <a href="#" class="btn btn-2">PROCESS</a> -->
+                  </div>
+                </div>
+                <div class="img img-1"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">글로오아시스 웹페이지 모작</span>
+                    <span class="content">작업기간 2021.06.15~6.25</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://codepen.io/sbyeony/full/wvJVLmb" target="_blank" class="btn btn-1">codepen
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-2"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">동아출판 웹페이지 모작</span>
+                    <span class="content">작업기간 2021.06.28~7.1</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://codepen.io/sbyeony/full/VwbLpNB" target="_blank" class="btn btn-1">codepen
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-3"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">미래에셋 웹페이지 모작</span>
+                    <span class="content">작업기간 2021.7.6~7.12</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://codepen.io/sbyeony/full/eYWBpjd" target="_blank" class="btn btn-1">codepen
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-4"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">타슈 어플리케이션 리디자인</span>
+                    <span class="content">작업기간 2021.8</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://notefolio.net/sbyeony/256395" target="_blank" class="btn btn-1">notefolio
+                      open</a>
+                    <a href="https://seoyoenkk68.github.io/tashu_redesign/" target="_blank" class="btn btn-2">process
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-5"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">야마하뮤직코리아 웹사이트 리디자인</span>
+                    <span class="content">작업기간 2021.9</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://notefolio.net/sbyeony/266043" target="_blank" class="btn btn-1">notefolio
+                      open</a>
+                    <a href="https://seoyoenkk68.github.io/yamaha_redeisgn/" target="_blank" class="btn btn-2">website
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-6"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">테슬라 반응형 모작 피그마 모작, 구현</span>
+                    <span class="content">작업기간 2021.10</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://www.figma.com/file/HnitnFgRHv695XdbF2vvZF/%ED%85%8C%EC%8A%AC%EB%9D%BC%EB%AA%A8%EC%9E%91"
+                      target="_blank" class="btn btn-1">figma
+                      open</a>
+                    <a href="https://codepen.io/sbyeony/pen/GRvryyG" target="_blank" class="btn btn-2">codepen
+                      open</a>
+                  </div>
+                </div>
+                <div class="img img-7"></div>
+              </li>
+              <li>
+                <div class="content-wrap flex flex-col justify-end">
+                  <div class="txt-wrap">
+                    <span class="title">lge.co.kr 반응형 상단바 모작</span>
+                    <span class="content">작업기간 2021.10</span>
+                  </div>
+                  <div class="btn-wrap flex justify-center">
+                    <a href="https://codepen.io/sbyeony/pen/MWpRZYB" target="_blank" class="btn btn-1">process open</a>
+                    <a href="https://codepen.io/sbyeony/pen/YzZMgmX" target="_blank" class="btn btn-2">codepen open</a>
+                  </div>
+                </div>
+                <div class="img img-8"></div>
+              </li>
+            </ul>
+          </li>
+          <li data-tb="tab-box-1__3__body">
             <ul class="artwork list flex">
               <li>
                 <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull1.png"
@@ -173,7 +332,11 @@ $pageCode="pf";
                 </a>
               </li>
               <li>
-                <div class="img empty"></div>
+                <a href="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artworkfull6.png"
+                  data-lightbox="image-1" data-title="overcomplex 로고 시안 2">
+                  <img class="aimg-1" src="https://rawcdn.githack.com/seoyoenkk68/pf-seoyeon/master/image/artwork6.png"
+                    alt="">
+                </a>
               </li>
               <li>
                 <div class="img empty"></div>
